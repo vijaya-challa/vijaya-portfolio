8067c39b9db711ed48d4d003dc5df4bf13f901bf
Vijaya Challa <vijayajay1814@gmail.com>
2023-03-27 02:40:49 +0200

    Don't refresh page on nav bar link redirect

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -2,18 +2,19 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { DarkModeToggle } from "react-dark-mode-toggle-2";
+import { Link } from 'react-router-dom';
 
 function MyNavbar({mode, isDarkMode, setIsDarkMode}) {
   return (
     <Navbar collapseOnSelect expand="md" bg={mode} variant={mode} sticky="top">
       <Container>
-        <Navbar.Brand href="/" className='vijayaLabel'>Vijaya</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" className='vijayaLabel'>Vijaya</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav"></Navbar.Toggle>
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="container-fluid justify-content-evenly">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/projects">Projects</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
           </Nav>
           <Nav>
           <DarkModeToggle onChange={setIsDarkMode} isDarkMode={isDarkMode} size="4rem" className="lightDarkMode"></DarkModeToggle>
